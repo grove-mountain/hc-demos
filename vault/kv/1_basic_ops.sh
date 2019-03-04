11ebe13380a5e9b982622640c4fb23ce90ac7869
Jake Lundberg <grove.mountain.github@nym.hush.com>
2019-03-03 23:36:16 -0800

    master: making easier to follow API calls

--- vault/kv/1_basic_ops.sh
+++ vault/kv/1_basic_ops.sh
@@ -5,6 +5,9 @@ green "This is mainly due to the fact secret will not be mount by default in the
 
 pe "vault secrets enable -path=secret -version=2 kv"
 
+green "This demo will read and write using the root token"
+yellow "This is not normal for most operations with Vault and only illustrates basic usage journeys"
+green "Putting in basic kv secrets and reading them out using CLI"
 pe "vault kv put secret/db1/beer/read-only username=anchor password=thebest"
 pe "vault kv get secret/db1/beer/read-only"
 pe "vault kv get -field=password secret/db1/beer/read-only"
@@ -14,6 +17,8 @@ pe "vault kv put secret/db1/finance/read-only username=warren password=feargreed
 pe "vault kv get secret/db1/finance/read-only"
 pe "vault kv get -field=password secret/db1/finance/read-only"
 
+
+green "Putting in basic kv secrets and reading them out using API"
 cat > payload.json << EOL
 {
   "data" : {
@@ -36,11 +41,19 @@ curl -s --header "X-Vault-Token:${VAULT_TOKEN}" \
     --request POST \
     --data @payload.json \
     ${VAULT_ADDR}/v1/secret/data/hitchhikers/drinks/best | jq .
+p
 
+cat << EOF
+curl -s --header "X-Vault-Token:\${VAULT_TOKEN}"
+    --request GET
+    \${VAULT_ADDR}/v1/secret/data/hitchhikers/drinks/best | jq .
+EOF
+p
 
-pe "vault kv get secret/hitchhikers/drinks/best"
-
-
+curl -s --header "X-Vault-Token:${VAULT_TOKEN}" \
+    --request GET \
+    ${VAULT_ADDR}/v1/secret/data/hitchhikers/drinks/best | jq .
+p
 
 cat > payload.json << EOL
 {
@@ -65,7 +78,18 @@ curl -s --header "X-Vault-Token:${VAULT_TOKEN}" \
     --request POST \
     --data @payload.json \
     ${VAULT_ADDR}/v1/secret/data/hitchhikers/life/theuniverse/everything | jq .
+p
 
+cat << EOF
+curl -s --header "X-Vault-Token:\${VAULT_TOKEN}"
+    --request GET
+    \${VAULT_ADDR}/v1/secret/data/hitchhikers/life/theuniverse/everything | jq .data.data
+EOF
+p
 
-pe "vault kv get secret/hitchhikers/life/theuniverse/everything"
+curl -s --header "X-Vault-Token:${VAULT_TOKEN}" \
+    --request GET \
+    ${VAULT_ADDR}/v1/secret/data/hitchhikers/life/theuniverse/everything | jq .data.data
+p
+
 
