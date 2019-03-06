2d2896ef82a6b8b4f57bb8d4a5825a314f5dc162
Jake Lundberg <grove.mountain.github@nym.hush.com>
2019-03-06 10:41:08 -0800

    master: adding in policy checks and fail conditions

--- vault/ldap_auth/3_memberOf_search.sh
+++ vault/ldap_auth/3_memberOf_search.sh
@@ -4,15 +4,33 @@ pe "vault secrets enable -path=vault_education kv"
 pe "vault secrets enable -path=terraform_education kv"
 
 pe "vault policy write vault_edu_full policies/vault_edu_full_policy.hcl"
+pe "vault policy read vault_edu_full"
 pe "vault policy write vault_edu_read policies/vault_edu_read_policy.hcl"
+pe "vault policy read vault_edu_read"
 
 pe "vault policy write tf_edu_full policies/tf_edu_full_policy.hcl"
+pe "vault policy read tf_edu_full"
+pe "vault policy write vault_edu_read policies/vault_edu_read_policy.hcl"
 pe "vault policy write tf_edu_read policies/tf_edu_read_policy.hcl"
+pe "vault policy read tf_edu_read"
 
 pe "vault write auth/ldap-mo/groups/cn=vault_education,ou=um_group,dc=hashidemos,dc=com policies=vault_edu_full,tf_edu_read"
 
 pe "vault write auth/ldap-mo/groups/cn=terraform_education,ou=um_group,dc=hashidemos,dc=com policies=tf_edu_full,vault_edu_read"
 
+pe "vault kv put vault_education/root password=voot"
+pe "vault kv put terraform_education/root password=toot"
 # Test Member Of logins and capabilities
+unset VAULT_TOKEN
 pe "vault login -method=ldap -path=ldap-mo/ username=yoko password=${USER_PASSWORD}"
-pe "vault write vault_education/db1 password=42zoo"
+pe "vault kv put vault_education/db1 password=42zoo"
+pe "vault kv get vault_education/db1"
+pe "vault kv put terraform_education/db1 password=42zoo"
+pe "vault kv get terraform_education/root"
+
+unset VAULT_TOKEN
+pe "vault login -method=ldap -path=ldap-mo/ username=rachel password=${USER_PASSWORD}"
+pe "vault kv put terraform_education/db1 password=42zoo"
+pe "vault kv get terraform_education/db1"
+pe "vault kv put vault_education/db1 password=42zoo"
+pe "vault kv get vault_education/root"
